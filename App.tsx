84374881dc42c7706435fa802166386c160de11a
Guillermo Rivas <gmo.rivas@tec.mx>
2024-10-08 07:31:49 -0600

    initial push

--- App.tsx
+++ App.tsx
@@ -1,5 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from "firebase/app";
+
+// REASON TO USE ENVIRONMENT VARIABLES
+// 1. Not uploading API keys / IDs of any sort into a repo
+// 2. Data might change depending on your dev environment 
+// -- api keys
+// -- server URLs
+// -- any other sort of credentials / validation 
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_MESSAGE_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 export default function App() {
   return (
